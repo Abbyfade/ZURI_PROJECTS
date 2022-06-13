0f9c69e1ed5bea0037580519d49826e9b1b4b239
abiola <60517156+Abbyfade@users.noreply.github.com>
2022-06-13 22:09:32 +0100

    Update main.py

--- RockPaperScissors/main.py
+++ RockPaperScissors/main.py
@@ -7,7 +7,6 @@ user = input("Enter your name: ")
 check = input("Enter yes to play or quit to end the game: ").lower()
 
 while check == "yes":
-    s = random.randint(0, 2)
     UserSelection = input("Enter R for Rock, P for Paper, or S for Scissors: ").upper()
 
     if UserSelection not in options:
@@ -16,36 +15,36 @@ while check == "yes":
 
     else:
         print("computer picks {}".format(options[s].title()))
-        if options[s] == UserSelection:
+        if random.choice(options) == UserSelection:
             print("It's a tie")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
 
-        elif options[s] == "R" and UserSelection == "S":
+        elif random.choice(options)  == "R" and UserSelection == "S":
             print("Computer wins!")
             print("Rock smashes Scissors")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
 
-        elif options[s] == "R" and UserSelection == "P":
+        elif random.choice(options)  == "R" and UserSelection == "P":
             print("You win!")
             print("Paper covers Rock")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
 
-        elif options[s] == "P" and UserSelection == "R":
+        elif random.choice(options) == "P" and UserSelection == "R":
             print("Computer wins!")
             print("Paper covers Rock")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
 
-        elif options[s] == "P" and UserSelection == "S":
+        elif random.choice(options) == "P" and UserSelection == "S":
             print("You win!")
             print("Scissors cuts Paper")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
 
-        elif options[s] == "S" and UserSelection == "R":
+        elif random.choice(options) == "S" and UserSelection == "R":
             print("You win!")
             print("Rock smashes Scissors")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
 
-        elif options[s] == "S" and UserSelection == "P":
+        elif random.choice(options)  == "S" and UserSelection == "P":
             print("Computer wins!")
             print("Scissors cuts Paper")
             check = input("Enter yes to keep playing or quit to end the game: ").lower()
